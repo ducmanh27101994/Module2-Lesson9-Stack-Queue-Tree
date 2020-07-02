4b23842b0e659b0bdbf8d3c86294ce3617656804
ducmanh27101994 <ducmanh27101994@gmail.com>
2020-07-02 13:31:05 +0700

    upbt

---
+++ [Bài tập] Cài đặt cấu trúc Stack/Stack.php
@@ -0,0 +1,69 @@
+<?php
+
+class Stack
+{
+    private $stack;
+    private $limit;
+
+    public function __construct($limit = 10)
+    {
+        $this->stack = array();
+        $this->limit = $limit;
+    }
+    function getStack(){
+        return $this->stack;
+    }
+
+    function isEmpty()
+    {
+        if (!empty($this->stack)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function push($data)
+    {
+        if (count($this->stack) < $this->limit) {
+            array_unshift($this->stack, $data);
+        }
+    }
+
+    function pop()
+    {
+        if ($this->isEmpty()) {
+            array_shift($this->stack);
+        }
+    }
+
+    function top()
+    {
+        return current($this->stack);
+    }
+}
+
+$stack = new Stack();
+$stack->push(3);
+$stack->push(6);
+$stack->push(2);
+$stack->push(10);
+$stack->push(13);
+$stack->pop();
+$stack->pop();
+$stack->pop();
+$stack->push(20);
+$stack->push(33);
+var_dump($stack->isEmpty());
+echo "<br>";
+$stack->pop();
+$stack->pop();
+$stack->pop();
+$stack->pop();
+var_dump($stack->isEmpty());
+
+
+
+echo "<pre>";
+print_r($stack->getStack());
+echo "</pre>";
